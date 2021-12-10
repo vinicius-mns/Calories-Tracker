ea60022289bf19cd7a9a5c86ac762d961a5a83a6
vinicius-mns <vinicius_mns@hotmail.com>
2021-12-10 01:40:28 -0300

    Adicionando Rotas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import Contente from './components/Content';
 
 function App() {
   return (
     <div> 
+      <Contente />
     </div>
   );
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from '../pages/Home';
+import Calendar_tracker from '../pages/Calendar_tracker';
+import Weigth_tracker from '../pages/Weight_tracker';
+
+export default function Rotas() {
+  return(
+    <div>
+      <Switch>
+        <Route path="/Calendar_tracker" component={ Calendar_tracker }/>
+        <Route path="/Weight_tracker" component={ Weigth_tracker }/>
+        <Route exact path="/" component={ Home } />
+      </Switch>
+    </div>
+  );
+}
