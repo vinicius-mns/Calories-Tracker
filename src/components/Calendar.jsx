ce704eb71205c8a7f0c27b3d4dd52ae346ed8415
vinicius-mns <vinicius_mns@hotmail.com>
2021-12-11 17:32:54 -0300

    Adicionando modal - Diario -

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import '../styles/calendar.css'
+import '../styles/modal.css';
 
 const december = ['x','x',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
   17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,'x','x']
@@ -8,12 +9,59 @@ const december = ['x','x',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
 const DAYS_OF_WEEk = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab', 'Dom']
 
 export default function Calendar() {
-  const [days, setDays] = useState(december)  
+  const [ days, setDays ] = useState(december)
+  const [ modal, setModal ] = useState(false)
+  const [ day, setDay ] = useState('')
+
+  function handleClick({target:{id}}) {
+    const day = id - 1 
+
+    setDay(day)
+    setModal(true)
+  }
+
+  function Modal() {
+    return(
+      <div className='modal'>
+        <div className='header'>
+          <h2>Dia {day}</h2>
+          <button>X</button>
+        </div>
+
+        <div className='main'>
+          <p>Diário</p>
+          <textarea name="diario" id="diario" cols="30" rows="10" placeholder='Comi 800kcal e
+          não fiz exercicios' />
+        </div>
+
+        <div className='final' >
+          <div className='result box'>
+            <div className='circulo' />
+          </div>
+
+          <div className='box'>
+            <button>Confirmar</button>
+            <button>Deletar</button>
+          </div>
+        </div>
+      </div>
+    )
+  }
 
   return(
     <div className='calendar'>
-      <div className='container'>{DAYS_OF_WEEk.map((days) => <button>{days}</button>)}</div>
-      <div className='container days'>{days.map((day) => <button>{day}</button> )}</div>
+      { modal && Modal() }
+      <div className='container'>
+        {
+          DAYS_OF_WEEk.map((days) => <button>{days}</button>)
+        }
+      </div>
+      <div className='container days'>
+        {
+          days.map((day, index) => <button onClick={ handleClick } id={index} >{day}</button> )
+        }
+      </div>
     </div>
   )
 }
+ 
