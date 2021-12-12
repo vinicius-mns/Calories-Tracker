8a926e43631cc3c0043409da2426ac4bf16e7730
vinicius-mns <vinicius_mns@hotmail.com>
2021-12-12 17:23:57 -0300

    organizando pagina Calendar

--- src/pages/CalendarTracker.jsx
+++ src/pages/CalendarTracker.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../styles/calendarTracker.css'
 import Calendar from '../components/Calendar';
-import Table from '../components/Table';
+// import Table from '../components/Table';
 
 export default function CalendarTracker() {
   return(
     <div className='calendarTracker'>
+      <h1>Calendário</h1>
+      <h2>Dezembro</h2>
       <Calendar />
-      <Table />
+      {/* <Table /> */}
     </div>
   );
 }
