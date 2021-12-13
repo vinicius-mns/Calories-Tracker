ebdcdd4db94a517bd3233fe14aa3e9f64e8806d8
vinicius-mns <vinicius_mns@hotmail.com>
2021-12-13 20:39:31 -0300

    Recriando guia

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,13 +3,25 @@ import '../styles/table.css'
 
 export default function Table() {
   return (
-    <div className="naosei" >
+    <div className="guideContainer" >
       <div className="guide">
-        <div className="color" />
+        <div className="gradientColor">
+          <div className="color" />
+          <div className="values">
+            <p>200 Kcal -</p>
+            <p>+ 1200 Kcal</p>
+          </div>
+        </div>
 
-        <div className="values">
-          <p>200 Kcal -</p>
-          <p>+ 1200 Kcal</p>
+        <div className="flowers">
+            <div className="floresJuntas">
+              <span className="cherry">🌸</span>
+              <p>Não fez exercícios</p>
+            </div>
+            <div className="floresJuntas">
+              <span className="cherry2">💮</span>
+              <p>Fez exercícios</p>
+            </div>
         </div>
       </div>
     </div>
