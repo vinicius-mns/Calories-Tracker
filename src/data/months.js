b7423b88efd3fc2ddcb1b907b7f2346f4bf4d642
vinicius-mns <vinicius_mns@hotmail.com>
2021-12-16 00:34:17 -0300

    Implementa meses dinamicos

--- src/data/months.js
+++ src/data/months.js
@@ -3,11 +3,16 @@ export const months = {
   xxi: {
     11: {
       month: 'Dezembro',
-      storage: 'december_tracker',
+    },
+    Dezembro: {
+      id: 11,
+      month: 'Dezembro',
+      storage: 'Dezembro_tracker_xxi',
       days: ['x','x',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
       17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,'x','x'],
     }
   },
+  
   
   // ano 2022
   xxii: {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
-import React,{useEffect} from 'react';
+import React,{ useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { setMonth } from '../slice'
-import { useDispatch } from 'react-redux';
-import '../styles/home.css';
 import { months } from '../data/months';
+import '../styles/home.css';
 
 export default function Home() {
-  const dispatch = useDispatch()
+  const [ month, setMonth ] = useState('nada')
   
   useEffect(() => {
     const date = {
@@ -25,13 +23,14 @@ export default function Home() {
         break;
     }
 
-    dispatch(setMonth(months[date.year][date.month]))
+    setMonth(months[date.year][date.month])
+
   },[])
 
   return(
     <div>
       <div className='card_component'>
-        <Link className='link' to='/calendar_tracker'><button>Calorias</button></Link>
+        <Link className='link' to={`/calendar_tracker/${ month.month }`}><button>Calorias</button></Link>
         <Link className='link' to='/weight_tracker'><button>Peso</button></Link>
         <Link className='link' to='/help'><button>Help</button></Link>
       </div>
