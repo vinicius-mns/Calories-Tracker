317226c2439bb09818e602e3de1242fd6026e95d
vinicius-mns <vinicius_mns@hotmail.com>
2021-12-17 11:04:37 -0300

    Adicionando mes favereiro

--- src/data/months.js
+++ src/data/months.js
@@ -18,9 +18,24 @@ export const months = {
   xxii: {
     0: {
       month: 'Janeiro',
-      storage: 'january',
+    },
+    Janeiro: {
+      id: 0,
+      month: 'Janeiro',
+      storage: 'Janeiro_tracker_xxi',
       days: ['x','x','x','x','x',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
-      17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,],
+      17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,'x','x','x','x','x','x'],
+    },
+
+    1: {
+      month: 'Fevereiro',
+    },
+    Fevereiro: {
+      id: 1,
+      month: 'Fevereiro',
+      storage: 'Fevereiro_tracker_xxii',
+      days: ['x',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
+      17,18,19,20,21,22,23,24,25,26,27,28,'x','x','x','x','x','x'],
     }
   }
 }
