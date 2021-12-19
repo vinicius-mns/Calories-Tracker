e000cb336f8ea3b9235d8495a99fe1fe1c4cc114
vinicius-mns <vinicius_mns@hotmail.com>
2021-12-18 22:43:50 -0300

    Aletera obj meses

--- src/data/months.js
+++ src/data/months.js
@@ -1,24 +1,14 @@
 export const months = {
-  // dano 2021
-  xxi: {
-    11: {
-      month: 'Dezembro',
-    },
+  // ano 2021
     Dezembro: {
       id: 11,
       month: 'Dezembro',
       storage: 'Dezembro_tracker_xxi',
       days: ['x','x',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
       17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,'x','x'],
-    }
-  },
-  
-  
-  // ano 2022
-  xxii: {
-    0: {
-      month: 'Janeiro',
     },
+
+  // ano 2022
     Janeiro: {
       id: 0,
       month: 'Janeiro',
@@ -27,17 +17,13 @@ export const months = {
       17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,'x','x','x','x','x','x'],
     },
 
-    1: {
-      month: 'Fevereiro',
-    },
     Fevereiro: {
       id: 1,
       month: 'Fevereiro',
       storage: 'Fevereiro_tracker_xxii',
       days: ['x',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
       17,18,19,20,21,22,23,24,25,26,27,28,'x','x','x','x','x','x'],
-    }
-  }
+    },
 }
 
 export const arrayMonths = [ 
