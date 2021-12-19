fc59a3707988e70eb0c6684b8a873d6776c6c4cf
vinicius-mns <vinicius_mns@hotmail.com>
2021-12-19 01:08:37 -0300

    Adiociona o mes Abriu

--- src/data/months.js
+++ src/data/months.js
@@ -31,7 +31,15 @@ export const months = {
       storage: 'Março_tracker_xxii',
       days: ['x',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
       17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,'x','x','x'],
-    }
+    },
+
+    Abriu: {
+      id: 3,
+      month: 'Abriu',
+      storage: 'Abriu_tracker_xxii',
+      days: ['x','x','x','x',1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
+      17,18,19,20,21,22,23,24,25,26,27,28,29,30,'x'], 
+    },
 }
 
 export const arrayMonths = [ 
