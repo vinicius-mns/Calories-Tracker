da8323ab17df547bd08a72c46506deecb1bb2001
vinicius-mns <vinicius_mns@hotmail.com>
2021-12-21 23:37:05 -0300

    Implementa a funcao que deleta todo local storage

--- src/components/ModalMonths.jsx
+++ src/components/ModalMonths.jsx
@@ -4,9 +4,32 @@ import { months } from "../data/months"
 
 export default function ModalMonths({ closeMonthModal, setNewMonth }) {
   const [ calendar ] = useState(Object.entries(months))
+  const [modal, setModal] = useState(false)
 
+  function delet() {
+    localStorage.clear()
+    setModal(false)
+  }
+
+  function Modal() {
+    return(
+      <div className="modalClearAll">
+        <div className="modalClearAllText">
+          Tem certeza que deseja apagar todas as informaçoes salvas em todos os meses ?
+          <strong>Essa ação é permanente e não ha reversão</strong>
+        </div>
+        <div className="modalClearAllButtons">
+          <button onClick={ () => setModal(false)} ><h3>Não</h3></button>
+          <button onClick={ delet } ><h3>Sim</h3></button>
+        </div>
+      </div>
+    )
+  }
     return (
       <div className='modalMonths'>
+        {
+          modal && Modal()
+        }
         <div className='titleModalMonth'>
           <h2>Escolha o mês que deseja</h2>
           <button onClick={closeMonthModal}><img src={cross} alt="fechar" /></button>
@@ -26,7 +49,7 @@ export default function ModalMonths({ closeMonthModal, setNewMonth }) {
             </div>
             )
           }
-          <button className="clearAll" onClick={ () => localStorage.clear() }>
+          <button className="clearAll" onClick={ () => setModal(true) }>
             <p>Apagar tudo</p>
           </button>
         </div>
