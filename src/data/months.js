c1dae0821e0539ea621623f42211410ec4b6e6f0
vinicius-mns <vinicius_mns@hotmail.com>
2022-01-01 19:03:51 -0300

    adicionando mais meses de marco a setembro no filtro `handleMonth(ano,mes)`

--- src/data/months.js
+++ src/data/months.js
@@ -70,6 +70,27 @@ export default function handleMonth(ano, mes) {
       case 1:
         mes = 'Fevereiro'
         break
+      case 2:
+        mes = 'Março'
+        break
+      case 3:
+        mes = 'Abriu'
+        break
+      case 4:
+        mes = 'Maio'
+        break
+      case 5:
+        mes = 'Junho'
+        break
+      case 6:
+        mes = 'Julho'
+        break
+      case 7:
+        mes = 'Agosto'
+        break
+      case 8:
+        mes = 'Setembro'
+        break
       case 11:
         mes = 'Dezembro'
         break
