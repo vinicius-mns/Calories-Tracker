65036ac42154dd1199a78a02d31f50c910ef90c4
vinicius-mns <vinicius_mns@hotmail.com>
2022-01-01 18:46:26 -0300

    inutilizando componente home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,24 @@
 import React,{ useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import handleMonth from '../data/months';
 import '../styles/home.css';
 
 export default function Home() {
-  const [ month, setMonth ] = useState('')
-  const [ year ] = useState( new Date().getFullYear() )
+  const  history = useHistory();
+  const [ year ] = useState( new Date().getFullYear() );
   
   useEffect(() => {
-    const mes = new Date().getMonth()
+    const mes = new Date().getMonth() // retornar um numero
+    const month = handleMonth(year, mes).month
 
-    setMonth(handleMonth(year, mes).month)
-  },[year])
+    history.push(`/calendar_tracker/${ month }/${ year }`)
+
+  },[history, year])
 
   return(
     <div>
       <div className='card_component'>
-        <Link className='link' to={`/calendar_tracker/${ month }/${ year }`}><button><h2>Calorias</h2></button></Link>
+        <Link className='link' to={'/repolho'}><button><h2>Calorias</h2></button></Link>
         <Link className='link' to='/weight_tracker'><button><h2>Peso</h2></button></Link>
         <Link className='link' to='/'><button><h2>Help</h2></button></Link>
       </div>
