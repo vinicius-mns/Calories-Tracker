9af5701736b87edc555ff88d5db9c80d15b72d25
vinicius-mns <vinicius_mns@hotmail.com>
2022-01-01 22:47:52 -0300

    refatorando completamente `Table`

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,7 +4,38 @@ import '../styles/table.css'
 export default function Table() {
   return (
     <div className="guideContainer" >
-      <div className="guide">
+      <div className="tableContainer">
+        <h2>Não fez exercicios 🌸</h2>
+        <div className="tableValues">
+          <div><span className="values aa naoT"/> <p>0 ~ 199 Kcal</p></div>
+          <div><span className="values bb naoT"/> <p>200 ~ 400 Kcal</p></div>
+          <div><span className="values cc naoT"/> <p>400 ~ 600 Kcal</p></div>
+          <div><span className="values dd naoT"/> <p>600 ~ 800 Kcal</p></div>
+          <div><span className="values ee naoT"/> <p>800 ~ 1000 Kcal</p></div>
+          <div><span className="values ff naoT"/> <p>1000 ~ 1200 Kcal</p></div>
+          <div><span className="values gg naoT"/> <p>1200 ~ 2500 Kcal</p></div>
+          <div><span className="values ii naoT"/> <p>+ 2500 Kcal</p></div>
+        </div>
+      </div>
+      <div className="tableContainer">
+        <h2>Fez exercicios 💮</h2>
+        <div className="tableValues">
+          <div><span className="values aa simT"/> <p>0 ~ 199 Kcal</p></div>
+          <div><span className="values bb simT"/> <p>200 ~ 400 Kcal</p></div>
+          <div><span className="values cc simT"/> <p>400 ~ 600 Kcal</p></div>
+          <div><span className="values dd simT"/> <p>600 ~ 800 Kcal</p></div>
+          <div><span className="values ee simT"/> <p>800 ~ 1000 Kcal</p></div>
+          <div><span className="values ff simT"/> <p>1000 ~ 1200 Kcal</p></div>
+          <div><span className="values gg simT"/> <p>1200 ~ 2500 Kcal</p></div>
+          <div><span className="values ii simT"/> <p>+ 2500 Kcal</p></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+// eslint-disable-next-line no-lone-blocks
+{/* <div className="guide">
         <div className="gradientColor">
           <div className="color" />
           <div className="values">
@@ -23,7 +54,4 @@ export default function Table() {
               <p>Fez exercícios</p>
             </div>
         </div>
-      </div>
-    </div>
-  )
-}
+      </div> */}
